a549963c258aff9f79201e05d0097527be8308b5
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-08-20 19:51:24 +0700

    Flask framework added

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("test-test")
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def test():
+    return flask.render_template('index.html')
+
+app.run(host='0.0.0.0', port=80)
